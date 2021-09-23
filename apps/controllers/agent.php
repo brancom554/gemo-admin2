9cc321b7dc06405eebcdd604124744f6e076ae27
cedric <ayedoun30121995@gmail.com>
2021-09-23 23:25:27 +0200

    'licenceView'

--- apps/controllers/agent.php
+++ apps/controllers/agent.php
@@ -2,7 +2,9 @@
 require_once(_APPS_PATH.'/classes/Database.php');
 
 // $sql = 'SELECT * FROM licences RIGHT JOIN users ON users.licence_id = licences.licence_id WHERE licences.licence_parent_id='.$_SESSION['licence'];
-$sql = 'SELECT * FROM licences LEFT JOIN users ON licences.licence_id = users.licence_id WHERE licences.licence_parent_id= '.$_SESSION['licence'];
+// $sql = 'SELECT * FROM licences LEFT JOIN users ON licences.licence_id = users.licence_id WHERE licences.licence_parent_id= 6';
+$sql = 'SELECT * FROM licences LEFT JOIN users ON licences.licence_id = users.licence_id WHERE licences.created_for_company_id='.$_SESSION['company'];
+
 
 $db = new Database();
 $data['licences']= $db->DisplayDataDb($sql);
