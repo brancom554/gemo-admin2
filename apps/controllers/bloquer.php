dede2f06bfb19bde5561f2aa9f8fb4d9ac5fd856
Arouna170 <hafizarouna@yahoo.com>
2021-09-28 11:03:14 +0200

    Update bloquer.php

--- apps/controllers/bloquer.php
+++ apps/controllers/bloquer.php
@@ -1,17 +1,17 @@
 <?php
 require _APPS_PATH.'/classes/Database.php';
 
-if (isset($url_array[4])) {
+if (isset($url_array[3])) {
     $db = new Database();
-    $conn = $db->connectDb();
+    
     $data=[
         'is_active'=> 0,
-        'id' => $url_array[4]
+        'id' => $url_array[3]
     ];
     $sql = 'UPDATE licences SET is_active=:is_active WHERE licence_id=:id';
-    $query = $conn->prepare($sql);
-    if ($query->execute($data)) {
-        header('Location:/super/licence');
+    $query = $db->InsertDb($sql,$data);
+    if (!is_array($query)) {
+        header('Location:/licences');
         exit;
     }
 }
